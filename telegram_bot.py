93a542eef2e6b2283d04ec4cae804049d17c1524
Aziz_Fayziev <fayziev978@mail.ru>
2025-11-27 14:23:49 +0500

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,11 +1,27 @@
+import os
 import telebot
-from config import Config
+import requests
 
-bot = telebot.TeleBot(Config.TELEGRAM_BOT_TOKEN)
-SERVER_URL = Config.SERVER_URL
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+SERVER_URL = os.getenv("SERVER_URL")  # Render domening
+
+bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start'])
-def start(msg):
-    bot.reply_to(msg, "Assalomu alaykum! Bot ishlayapti.")
+def start(message):
+    chat_id = message.chat.id
+    username = message.from_user.username
 
-# Backend bilan bog‘lanish uchun webhook bo‘lsa ham shu yerga yoziladi
+    bot.reply_to(message, "AF Imperiya tizimiga muvaffaqiyatli bog'landingiz!")
+
+    # serverga chat_id yuborish
+    try:
+        requests.post(f"{SERVER_URL}/api/save_chat_id", json={
+            "username": username,
+            "chat_id": chat_id
+        })
+    except:
+        pass
+
+print("Telegram bot ishga tushdi...")
+bot.infinity_polling()
