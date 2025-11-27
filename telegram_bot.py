651c6c0c524b2434c193e1f8d79ecacd62094d28
Aziz_Fayziev <fayziev978@mail.ru>
2025-11-27 11:05:14 +0500

    Create telegram_bot.py

---
+++ telegram_bot.py
@@ -0,0 +1,25 @@
+import os
+import telebot
+import requests
+
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+SERVER_URL = os.getenv("SERVER_URL")  # https://af-imperiya.onrender.com
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    chat_id = message.chat.id
+    username = message.from_user.username
+
+    bot.reply_to(message, "Assalomu alaykum! Siz AF IMPERIYA tizimi bilan birikdingiz.")
+
+    # serverga chat_id ni yuborish
+    requests.post(f"{SERVER_URL}/api/save_chat_id", json={
+        "username": username,
+        "chat_id": chat_id
+    })
+
+print("Telegram bot running...")
+bot.infinity_polling()
