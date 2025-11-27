b80e968449114496edcf4a2cb3a2de9fafc8d57c
Aziz_Fayziev <fayziev978@mail.ru>
2025-11-27 12:58:15 +0500

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,22 +1,11 @@
 import telebot
-import requests
-from config import TELEGRAM_BOT_TOKEN, SERVER_URL
+from config import Config
 
-bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
+bot = telebot.TeleBot(Config.TELEGRAM_BOT_TOKEN)
+SERVER_URL = Config.SERVER_URL
 
-# /start komandasi
 @bot.message_handler(commands=['start'])
-def start(message):
-    chat_id = message.chat.id
-    username = message.from_user.username  # telegram @username
+def start(msg):
+    bot.reply_to(msg, "Assalomu alaykum! Bot ishlayapti.")
 
-    bot.reply_to(message, "AF Imperiya botiga muvaffaqiyatli ulandingiz!")
-
-    # BACKENDGA yuboramiz
-    try:
-        requests.post(f"{SERVER_URL}/api/save_chat_id", json={
-            "username": username,
-            "chat_id": chat_id
-        })
-    except Exception as e:
-        print("Chat ID yuborishda xato:", e)
+# Backend bilan bog‘lanish uchun webhook bo‘lsa ham shu yerga yoziladi
