b709fb3eac8db61614ffc1fd7d1147eb0317d6c2
Aziz_Fayziev <fayziev978@mail.ru>
2025-11-27 12:32:01 +0500

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -10,18 +10,16 @@ bot = telebot.TeleBot(BOT_TOKEN)
 @bot.message_handler(commands=['start'])
 def start(message):
     chat_id = message.chat.id
-    username = message.from_user.username
+    username = message.from_user.username   # @username olib beradi
 
     bot.reply_to(message, "AF Imperiya tizimiga muvaffaqiyatli bog'landingiz!")
 
-    # serverga chat_id yuborish
     try:
         requests.post(f"{SERVER_URL}/api/save_chat_id", json={
             "username": username,
             "chat_id": chat_id
         })
-    except:
-        pass
+    except Exception as e:
+        print("Error:", e)
 
-print("Telegram bot ishga tushdi...")
 bot.infinity_polling()
