6079fa094ae5924ee59b209d7fdb750c80a65620
Aziz_Fayziev <fayziev978@mail.ru>
2025-11-27 12:55:50 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,15 +3,11 @@ import os
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "super-secret-key")
 
-    # Database
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL",
         "sqlite:///instance/af_imperiya.db"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # Telegram bot
     TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
-
-    # Render domain (webhook uchun)
     SERVER_URL = os.environ.get("SERVER_URL")
