c0c2c938bb93ab90c5081fdcbeb3c03bcb4f59c9
Aziz_Fayziev <fayziev978@mail.ru>
2025-11-27 11:08:51 +0500

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -3,23 +3,25 @@ import telebot
 import requests
 
 BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+SERVER_URL = os.getenv("SERVER_URL")  # Render domening
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
-SERVER_URL = os.getenv("SERVER_URL")  # https://af-imperiya.onrender.com
-
-@bot.message_handler(commands=["start"])
+@bot.message_handler(commands=['start'])
 def start(message):
     chat_id = message.chat.id
     username = message.from_user.username
 
-    bot.reply_to(message, "Assalomu alaykum! Siz AF IMPERIYA tizimi bilan birikdingiz.")
+    bot.reply_to(message, "AF Imperiya tizimiga muvaffaqiyatli bog'landingiz!")
 
-    # serverga chat_id ni yuborish
-    requests.post(f"{SERVER_URL}/api/save_chat_id", json={
-        "username": username,
-        "chat_id": chat_id
-    })
+    # serverga chat_id yuborish
+    try:
+        requests.post(f"{SERVER_URL}/api/save_chat_id", json={
+            "username": username,
+            "chat_id": chat_id
+        })
+    except:
+        pass
 
-print("Telegram bot running...")
+print("Telegram bot ishga tushdi...")
 bot.infinity_polling()
