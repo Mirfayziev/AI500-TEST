972a3feed0a1540d2040ec5bd933c16cee344c03
Aziz_Fayziev <fayziev978@mail.ru>
2025-11-27 12:39:40 +0500

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,25 +1,22 @@
-import os
 import telebot
 import requests
+from config import TELEGRAM_BOT_TOKEN, SERVER_URL
 
-BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-SERVER_URL = os.getenv("SERVER_URL")  # Render domening
+bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
 
-bot = telebot.TeleBot(BOT_TOKEN)
-
+# /start komandasi
 @bot.message_handler(commands=['start'])
 def start(message):
     chat_id = message.chat.id
-    username = message.from_user.username   # @username olib beradi
+    username = message.from_user.username  # telegram @username
 
-    bot.reply_to(message, "AF Imperiya tizimiga muvaffaqiyatli bog'landingiz!")
+    bot.reply_to(message, "AF Imperiya botiga muvaffaqiyatli ulandingiz!")
 
+    # BACKENDGA yuboramiz
     try:
         requests.post(f"{SERVER_URL}/api/save_chat_id", json={
             "username": username,
             "chat_id": chat_id
         })
     except Exception as e:
-        print("Error:", e)
-
-bot.infinity_polling()
+        print("Chat ID yuborishda xato:", e)
